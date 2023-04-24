49815f80439cd7b93006c5467702c48213761b7b
BigBobandy <jdykes207@gmail.com>
2023-04-24 18:07:26 -0400

    added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Grabbing html elements from the DOM
 const beginBtn = document.getElementById("beginBtn");
 const inputElement = document.getElementById("userInput");
 const form = document.getElementById("inputForm");
@@ -5,64 +6,73 @@ const container = document.querySelector(".container");
 const results = document.getElementById("resultContainer");
 const searchButton = document.getElementById("searchBtn");
 
+// Set the size of the array to 21
 let size = 21;
+// Initialize an array of size 21
 let array = [size];
 
+// Add an event listener to the begin button for a click event
 beginBtn.addEventListener("click", () => {
+  // Remove the "hide" class from the inputElement and searchButton
   inputElement.classList.remove("hide");
   searchButton.classList.remove("hide");
+  // Remove the begin button
   beginBtn.remove();
-  //Creating the element with directions and appending it
+
+  // Create an element with directions and append it to the container
   const directions = document.createElement("h4");
   directions.innerHTML =
     "Select a number from the array below to determine which search algorithm, binary or sequential, can more efficiently find your chosen number.";
   container.append(directions);
 
-  //Calling the function that populates the array with 20 random numbers from 1-99 then the list function puts them in a list element and appends them
+  // Call the function that populates the array with 20 random numbers from 1-99, then the list function puts them in a list element and appends them
   populateArray(array, size);
   list();
 
-  //Creating the element for the sequential search results
+  // Create an element for the sequential search results and append it to the results element
   const seqResult = document.createElement("h4");
   seqResult.setAttribute("id", "seqResult");
   seqResult.classList.add("result");
   results.append(seqResult);
 
-  //Creating the element for the binary search results
+  // Create an element for the binary search results and append it to the results element
   const binResult = document.createElement("h4");
   binResult.setAttribute("id", "binResult");
   binResult.classList.add("result");
   results.append(binResult);
 });
 
-//Event listener for the search button that will execute the lines of code when clicked
+// Add an event listener to the search button for a click event
 searchButton.addEventListener("click", () => {
+  // Remove the "hide" class from the results element
   results.classList.remove("hide");
+  // Get the user input value
   const input = document.getElementById("userInput").value;
-  //First the array is sorted in ascending order
+  // Sort the array in ascending order
   sortArray(array, size);
-  //Then the function for the sequential search of the array is called
+  // Call the sequential search function with the array, size, and input
   seqSearch(array, size, input);
-  //Then the function for binary search is called
+  // Call the binary search function with the array, size, and input
   binarySearch(array, size, input);
-  //Then the function to get the results of the binary search is called
+  // Call the function to get the results of every search
   getResults();
-  //The number entered by the user is cleared so that it cannot be spammed
+  // Reset the user input form
   form.reset();
 });
 
-//This function calls the random function and populates the array with 20 random numbers ranging from 1-99
+// This function calls the random function and populates the array with 20 random numbers ranging from 1-99
 function populateArray(array, size) {
   for (let i = 0; i < size - 1; i++) {
     array[i] = random();
   }
 }
 
+// Generate a random number between 1 and 99
 function random() {
   return 1 + Math.floor(Math.random() * 99);
 }
 
-//The list function creates a unordered list element and adds every number of the array to the list then appends it
+// The list function creates an unordered list element and adds every number of the array to the list, then appends it to the container
 function list() {
   const listContainer = document.createElement("ul");
   listContainer.classList.add("list");
@@ -74,6 +84,7 @@ function list() {
   });
 }
 
+// Sort the array in ascending order using the bubble sort
 function sortArray(array, size) {
   maxElement = size - 1;
 
@@ -88,6 +99,7 @@ function sortArray(array, size) {
   }
 }
 
+// Perform binary search on the sorted array to find the input value
 function binarySearch(array, size, input) {
   let start = 0;
   let end = size - 1;
@@ -111,6 +123,7 @@ function binarySearch(array, size, input) {
   return [found, searches];
 }
 
+// Get the results of the searches and display them
 function getResults() {
   const input = document.getElementById("userInput").value;
   const binResult = document.getElementById("binResult");
@@ -128,6 +141,7 @@ function getResults() {
   binResult.innerHTML = result;
 }
 
+// Perform sequential search on the array to find the input value
 function seqSearch(array, size, input) {
   const seqResult = document.getElementById("seqResult");
   let found = false;
@@ -145,6 +159,8 @@ function seqSearch(array, size, input) {
       searches += 1;
     }
   }
+
+  return searches;
 
   if (found == true && searches == 1) {
     result = `It took ${searches} search using the sequential search algorithm to find ${input}.`;
